2fdefb716caa6611ee300851cc172bb086ff0411
Amir Sohail <amirsohail335@gmail.com>
2023-03-25 22:14:23 +0530

    committed

--- components/__tests__/sum.test.js
+++ components/__tests__/sum.test.js
@@ -1,10 +1,13 @@
 import { sum } from "../sum";
 
-// test("Check sum of 2 positive numbers", () => {
-//   expect(sum(2, 5)).toBe(5);
-// });
-test("check sum of 2 numbers ", () => {
-  expect(sum(2, 3)).toBe(5);
-  expect(sum(-2, 3)).toBe(1);
-  expect(sum()).toBe("Please check argument");
+test("Check sum of 2 positive numbers", () => {
+  expect(sum(2, 5)).toBe(7);
 });
+
+
+
+// test("check sum of 2 numbers ", () => {
+//   expect(sum(2, 3)).toBe(5);
+//   expect(sum(-2, 3)).toBe(1);
+//   expect(sum()).toBe("Please check argument");
+// });
